7bbd98bf1fbfdbfb97e98afacaa20d6107f326e1
jogendra20 <jogendranathreddy20@gmail.com>
2026-05-18 16:07:36 +0530

    auto: scripts/auto_20260518_103735.py via Nexus

---
+++ scripts/auto_20260518_103735.py
@@ -0,0 +1,23 @@
+import os
+from playwright.sync_api import sync_playwright
+
+def scrape_books():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            args=[
+                '--no-sandbox',
+                '--disable-dev-shm-usage'
+            ]
+        )
+        page = browser.new_page()
+        page.goto('https://books.toscrape.com')
+
+        titles = page.locator('.product_pod h3 a').all()[:5]
+        for title in titles:
+            print(title.get_attribute('title'))
+
+        browser.close()
+
+if __name__ == '__main__':
+    scrape_books()
